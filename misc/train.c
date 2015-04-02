393712b6e8965575024231c03b46bcd4fd816c59
Tommy Unger <tommyu@bu.edu>
2015-04-02 11:45:22 -0400

    prep for package branch

--- misc/train.c
+++ misc/train.c
@@ -7,15 +7,15 @@ void build_committee_average(struct fann *ann, struct fann *bnn, struct fann *cn
 }
 int main(){
   const unsigned int num_input = 2;
-  const unsigned int hid = 1;
-  const unsigned int num_output = 1;
+  const unsigned int hid = 2;
+  const unsigned int num_output = 2;
   const unsigned int num_layers = 3;
   const float desired_error = (const float) 0.001;
-  const unsigned int max_epochs = 15;
+  const unsigned int max_epochs = 25;
   const unsigned int epochs_between_reports = 0;
-  struct fann_train_data *data = fann_read_train_from_file("or.dat");
+  struct fann_train_data *data = fann_read_train_from_file("copy.dat");
   struct fann *ann = fann_create_standard(num_layers, num_input, hid, num_output);
-  struct fann *bnn = fann_create_standard(num_layers, num_input, hid+1, num_output);  
+  struct fann *bnn = fann_create_standard(num_layers, num_input, hid, num_output);  
   //  struct fann *cnn = fann_create_standard(num_layers + 1, num_input,hid, 2*num_output, num_output);  
 
   int i;
@@ -27,17 +27,26 @@ int main(){
   //Check Performance
 
   printf("\nPerformance on training set ann \n");
-  for(i = 0; i < fann_length_train_data(data); i++, inputData+=2){
+  for(i = 0; i < fann_length_train_data(data); i++, inputData+=2, outputData+=2){
     calc_out = fann_run(ann, inputData);
-    printf("out actual: %f\toutcalculated: %f \tdiff: %f  \n", outputData[i], *calc_out, (outputData[i]- *calc_out) );
+    printf("out actual: %f\toutcalculated: %f \tdiff: %f  ", outputData[0], calc_out[0], (outputData[0]- calc_out[0]) );
+    printf("out actual: %f\toutcalculated: %f \tdiff: %f  \n", outputData[1], calc_out[1], (outputData[1]- calc_out[1]) );
   }
+  inputData = *data->input; outputData = *data->output;
+  printf("\nPerformance on training set bnn \n");
+  for(i = 0; i < fann_length_train_data(data); i++, inputData+=2, outputData+=2){
+    calc_out = fann_run(bnn, inputData);
+    printf("out actual: %f\toutcalculated: %f \tdiff: %f  ", outputData[0], calc_out[0], (outputData[0]- calc_out[0]) );
+    printf("out actual: %f\toutcalculated: %f \tdiff: %f  \n", outputData[1], calc_out[1], (outputData[1]- calc_out[1]) );
+  }
+  /*
   inputData = *data->input; outputData = *data->output;
   printf("\nPerformance on training set bnn \n");
   for(i = 0; i < fann_length_train_data(data); i++, inputData+=2){
     calc_out = fann_run(bnn, inputData);
     printf("out actual: %f\toutcalculated: %f \tdiff: %f  \n", outputData[i], *calc_out, (outputData[i]- *calc_out) );
   }
-
+  */
   /*
   cnn->weights[0] = ann->weights[0]; 
   cnn->weights[1] = ann->weights[1]; 
--- readme.txt
+++ readme.txt
@@ -1,2 +1,3 @@
 exe contains code for combining and evaluating nets, needs to be consolidated.
-vote_example contains necessary code to run the combining code on an example.
+vote_example contains necessary code to run the combining nets on a per output bit basis.
+committee_example contains code for averaging the output of two networks.  
